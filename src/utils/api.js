2d7c99ebc98c06d885e8e0b1e61ad915db453545
Bhakti-Panchal-13 <bhaktipanchal13g@gmail.com>
2025-07-05 23:39:10 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api', // 
+  baseURL: 'https://document-signature-app-server-hb3x.onrender.com/api', // 
   withCredentials: true, // ✅ Send cookies with each request
 });
 
